94157c8522617237f39a744eeced5bab2f9ea65a
William0832 <squall832@gmail.com>
2022-08-23 16:01:00 +0800

    🌈 style: confirmOrder Ui ok

--- src/pages/ConfirmOrder.vue
+++ src/pages/ConfirmOrder.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
-.p-2.pt-3.h-100
+.p-2.pt-3.h-100.wrapper.d-flex.flex-column
   TopToHome
+
   h3.border-bottom.border-secondary.pb-2 確認餐點
+
   .time.p-2.d-flex.align-items-start
     icon(:icon="['far', 'fa-clock']").pe-3.pt-1
     .d-block
@@ -12,28 +14,29 @@
       .form-check
         input.form-check-input#radio-pre(type="radio" v-model="isRightNowOrder" :value="false")
         label.form-check-label(for="radio-pre") 預定日期
-
   .date-select(v-if="!isRightNowOrder").px-2
     .input-box.d-flex.mb-2
       label.form-label.me-3
         icon(:icon="['fas', 'fa-calendar-day']")
       input.form-control(type="date" v-model="selectedDate")
     .ms-1.text-danger 說明: 我們一般只在週末中午營業, 預定完成後請在與店家確認, 謝謝您！
-  .special-box
+
+  .special-box.mt-1
     label.form-label(for="special") 特殊需求
     textarea.form-control#special(v-model="tempOrder.special")
+
   .detail-box
     h4.mt-2.border-bottom.border-secondary.p-2 訂單明細
     ul.list-group
       li.list-group-item(v-for="e in Array(5)") 
-      .head.d-flex.justify-content-between 
-        .title item1
-        .price $100
-      .body.info test information~~~~
-      .footer.actions.d-flex.justify-content-between 
-        //- AmountCtrl(v-model="amount")
-        .amount-ctrl -1+
-        button.btn.btn-sm.btn-outline-danger 刪除
+        .head.d-flex.justify-content-between 
+          .title item1
+          .price $100
+        .body.info test information~~~~
+        .footer.actions.d-flex.justify-content-between 
+          //- AmountCtrl(v-model="amount")
+          .amount-ctrl -1+
+          button.btn.btn-sm.btn-outline-danger 刪除
 
 
 </template>
@@ -80,3 +83,17 @@ const isRightNowOrder = computed({
   }
 })
 </script>
+
+<style lang="sass">
+.wrapper
+  height: calc(100% - 70px) !important
+.detail-box
+  display: flex
+  flex-direction: column
+  flex: 1
+  overflow: auto
+  ul
+    flex: 1
+    overflow: auto
+
+</style>
