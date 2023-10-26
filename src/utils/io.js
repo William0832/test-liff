b9ba1a388a43f41306b3a1593aa1fa7287e907b2
William0832 <squall832@gmail.com>
2023-10-26 12:33:57 +0800

    ✨ feat: add socket-io

--- src/utils/io.js
+++ src/utils/io.js
@@ -1,5 +1,24 @@
-import io from 'socket.io-client'
-export const useSocket = () => {
-  const socket = io(import.meta.env.VITE_SOCKET_IO_URL)
-  return { socket }
-}
+import { io } from 'socket.io-client'
+import { useAdminOrderStore } from '../stores/adminOrder'
+
+const URL = import.meta.env.VITE_SOCKET_IO_URL
+export const socket = io(URL, {
+  transportOptions: {
+    websocket: {
+      path: URL
+    }
+  }
+})
+
+socket.on('connect', () => {
+  console.log('connected')
+})
+
+socket.on('MSG', (payload) => {
+  const adminStore = useAdminOrderStore()
+  adminStore.addSocketAlert(payload)
+})
+
+socket.on('disconnect', () => {
+  console.log('disconnect')
+})
